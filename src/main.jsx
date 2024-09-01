4f802fce05c13d427dd7cf433cca5010f9782674
TanujaGajavelli <tanujasai20@gmail.com>
2024-09-01 14:21:04 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { ChakraBaseProvider, ChakraProvider } from '@chakra-ui/react'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-  <Router>
+  //<React.StrictMode>
+  <BrowserRouter>
 
      <App />
-  </Router>
+  </BrowserRouter>
    
-  </React.StrictMode>,
+  //</React.StrictMode>,
 )
